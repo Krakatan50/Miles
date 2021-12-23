91a2616d27c5165c79a454b19aa2bf8a196dd41b
Krakatan50 <krakatan@yandex.ru>
2021-12-23 23:25:04 +0300

    Прога работает

---
+++ src/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticket = 100; // стоимость билета
+        int rubles = 20;
+
+        System.out.println("Ваш бонус: " + ticket / rubles + "мили");
+    }
+}
